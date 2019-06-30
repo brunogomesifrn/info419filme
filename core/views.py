4c87bb77b4efd744447af852393ea5aa74062dc5
AnaPaula <apsantana026@gmail.com>
2019-06-30 20:33:51 -0300

    views das pags das fotos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+def foto1(request):
+	return render(request, 'foto1.html')
+
+def foto2(request):
+	return render(request, 'foto2.html')
+
+def foto3(request):
+	return render(request, 'foto3.html')
+
+def foto4(request):
+	return render(request, 'foto4.html')
+
+def foto5(request):
+	return render(request, 'foto5.html')
+
+def foto6(request):
+	return render(request, 'foto6.html')
+
+def foto7(request):
+	return render(request, 'foto7.html')
+
+def foto8(request):
+	return render(request, 'foto8.html')
